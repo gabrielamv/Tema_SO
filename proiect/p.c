dd9b9edd9f96227d17836c2607c433f9c44fea95
Gabriela <gabrielavieriu01@gmail.com>
2023-12-08 15:12:01 +0200

    Proiect 8-partea 1

--- proiect/p.c
+++ proiect/p.c
@@ -14,9 +14,11 @@
 #include <time.h>
 #include <dirent.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #define BUFFSIZE 4096
 
-int inaltime,latime;
+int inaltime, latime;
 int identificator;
 int dimensiune, dimensiune_legatura;
 char data[100];
@@ -26,12 +28,13 @@ char drepturi[4][4];
 /*Citeste latimea si lungimea unui fisier BMP*/
 void citireBMP(char *fisier)
 {
-    int fd=open(fisier, S_IRUSR);
-    if(fd==-1)
+    int fd = open(fisier, S_IRUSR);
+    if (fd == -1)
     {
         perror("Fisierul nu a fost gasit! Mai cauta.\n");
-        exit(0);    }
-    
+        exit(0);
+    }
+
     lseek(fd, 18, SEEK_SET);
     read(fd, &latime, 4);
     read(fd, &inaltime, 4);
@@ -42,81 +45,82 @@ void citireBMP(char *fisier)
 int citireInfoFisier(char *fisier)
 {
     struct stat inf;
-    int rez=stat(fisier,&inf);
-    if(rez==-1)
+    int rez = stat(fisier, &inf);
+    if (rez == -1)
     {
         perror("Fisierul nu a fost gasit! Mai cauta.\n");
         exit(0);
     }
-    
+
     int tip = -1;
-    if(inf.st_mode | S_IFDIR)
+    if (inf.st_mode | S_IFDIR)
         tip = 3;
-    if(inf.st_mode | S_IFREG){
-        tip=0;
+    if (inf.st_mode | S_IFREG)
+    {
+        tip = 0;
         int l = strlen(fisier);
-        if(fisier[l-4]=='.' || fisier[l-3]=='b' || fisier[l-2]=='m' || fisier[l-1]=='p')
-            tip=1; 
+        if (fisier[l - 4] == '.' || fisier[l - 3] == 'b' || fisier[l - 2] == 'm' || fisier[l - 1] == 'p')
+            tip = 1;
     }
 
-    if(inf.st_mode | S_IFLNK)
+    if (inf.st_mode | S_IFLNK)
         tip = 2;
 
-
-    identificator=inf.st_uid;
-    dimensiune=inf.st_size;
-    nrLegaturi=inf.st_nlink;
+    identificator = inf.st_uid;
+    dimensiune = inf.st_size;
+    nrLegaturi = inf.st_nlink;
     time_t raw_time = inf.st_mtime;
-    struct tm *timpCorect=localtime(&raw_time);
+    struct tm *timpCorect = localtime(&raw_time);
     sprintf(data, "%02d.%02d.%d %02d:%02d",
-    timpCorect->tm_mday,
-    timpCorect->tm_mon,
-    timpCorect->tm_year,
-    timpCorect->tm_hour,
-    timpCorect->tm_min);
+            timpCorect->tm_mday,
+            timpCorect->tm_mon,
+            timpCorect->tm_year,
+            timpCorect->tm_hour,
+            timpCorect->tm_min);
 
-    if(inf.st_mode&S_IRUSR)
-        drepturi[0][0]='R';
+    if (inf.st_mode & S_IRUSR)
+        drepturi[0][0] = 'R';
     else
-        drepturi[0][0]='-';
-    if(inf.st_mode&S_IWUSR)
-        drepturi[0][1]='W';
+        drepturi[0][0] = '-';
+    if (inf.st_mode & S_IWUSR)
+        drepturi[0][1] = 'W';
     else
-        drepturi[0][1]='-';
-    if(inf.st_mode&S_IXUSR)
-        drepturi[0][2]='E';
+        drepturi[0][1] = '-';
+    if (inf.st_mode & S_IXUSR)
+        drepturi[0][2] = 'E';
     else
-        drepturi[0][2]='-';
+        drepturi[0][2] = '-';
 
-    if(inf.st_mode&S_IRGRP)
-        drepturi[1][0]='R';
+    if (inf.st_mode & S_IRGRP)
+        drepturi[1][0] = 'R';
     else
-        drepturi[1][0]='-';
-    if(inf.st_mode&S_IWGRP)
-        drepturi[1][1]='W';
+        drepturi[1][0] = '-';
+    if (inf.st_mode & S_IWGRP)
+        drepturi[1][1] = 'W';
     else
-        drepturi[1][1]='-';
-    if(inf.st_mode&S_IXGRP)
-        drepturi[1][2]='E';
+        drepturi[1][1] = '-';
+    if (inf.st_mode & S_IXGRP)
+        drepturi[1][2] = 'E';
     else
-        drepturi[1][2]='-';
+        drepturi[1][2] = '-';
 
-    if(inf.st_mode&S_IROTH)
-        drepturi[2][0]='R';
+    if (inf.st_mode & S_IROTH)
+        drepturi[2][0] = 'R';
     else
-        drepturi[2][0]='-';
-    if(inf.st_mode&S_IWOTH)
-        drepturi[2][1]='W';
+        drepturi[2][0] = '-';
+    if (inf.st_mode & S_IWOTH)
+        drepturi[2][1] = 'W';
     else
-        drepturi[2][1]='-';
-    if(inf.st_mode&S_IXOTH)
-        drepturi[2][2]='E';
+        drepturi[2][1] = '-';
+    if (inf.st_mode & S_IXOTH)
+        drepturi[2][2] = 'E';
     else
-        drepturi[2][2]='-';
-    
-    if(tip == 2){
-        rez=lstat(fisier,&inf);
-        if(rez==-1)
+        drepturi[2][2] = '-';
+
+    if (tip == 2)
+    {
+        rez = lstat(fisier, &inf);
+        if (rez == -1)
         {
             perror("Fisierul nu a fost gasit! Mai cauta.\n");
             exit(0);
@@ -126,119 +130,166 @@ int citireInfoFisier(char *fisier)
     return tip;
 }
 
-
 /*adauga in fisierul statistica.txt informatiile despre fisier/director
 TIP: 0=normal, 1=bmp, 2=leg simbolica, 3=director*/
-void scriereStatistici(char *fisier, int tip)
+int scriereStatistici(char *cale, char *fisier, int tip)
 {
-    int fd=open ("statistica.txt", O_WRONLY| O_APPEND);
-    if(fd==-1)
+    int nr_linii = 0;
+    int fd = open(cale, O_CREAT|O_RDWR|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
+    if (fd == -1)
     {
         perror("Eroare scriere statistici.");
     }
     int n;
-    char buff[100]; //lungimea liniei
+    char buff[100]; // lungimea liniei
 
-    if(tip == 0 || tip==1)
-        n=sprintf(buff, "Nume fisier: %s\n", fisier);
-    else if(tip == 2)
-        n=sprintf(buff, "Nume legatura: %s\n", fisier);
+    if (tip == 0 || tip == 1)
+        n = sprintf(buff, "Nume fisier: %s\n", fisier);
+    else if (tip == 2)
+        n = sprintf(buff, "Nume legatura: %s\n", fisier);
     else
-        n=sprintf(buff, "Nume director: %s\n", fisier);
+        n = sprintf(buff, "Nume director: %s\n", fisier);
     write(fd, buff, n);
+    nr_linii++;
 
-    if(tip == 1){
-        n=sprintf(buff, "Inaltime: %d\n", inaltime);
+    if (tip == 1)
+    {
+        n = sprintf(buff, "Inaltime: %d\n", inaltime);
         write(fd, buff, n);
-        n=sprintf(buff, "Latime: %d\n",latime);
+        nr_linii++;
+        n = sprintf(buff, "Latime: %d\n", latime);
         write(fd, buff, n);
+        nr_linii++;
     }
 
-    if(tip ==0 || tip == 1){
-        n=sprintf(buff, "Dimensiune: %d\n", dimensiune);
+    if (tip == 0 || tip == 1)
+    {
+        n = sprintf(buff, "Dimensiune: %d\n", dimensiune);
         write(fd, buff, n);
+        nr_linii++;
     }
-    if(tip ==2){
-        n=sprintf(buff, "Dimensiune: %d\n", dimensiune_legatura);
+    if (tip == 2)
+    {
+        n = sprintf(buff, "Dimensiune: %d\n", dimensiune_legatura);
         write(fd, buff, n);
-        n=sprintf(buff, "Dimensiune fisier: %d\n", dimensiune);
+        nr_linii++;
+        n = sprintf(buff, "Dimensiune fisier: %d\n", dimensiune);
         write(fd, buff, n);
-    }
-
-    if(tip != 2){
-        n=sprintf(buff, "Identificator utilizator: %d\n", identificator);
-        write(fd, buff, n);
-    }
-
-    if(tip == 0 || tip == 1){
-        n=sprintf(buff, "Timpul: %s\n", data);
-        write(fd, buff, n);
-
-        n=sprintf(buff, "Contor legaturi: %d\n", nrLegaturi);
-        write(fd, buff, n);
+        nr_linii++;
     }
 
-    n=sprintf(buff, "Drepturi acces user: %s\n", drepturi[0]);
+    if (tip != 2)
+    {
+        n = sprintf(buff, "Identificator utilizator: %d\n", identificator);
+        write(fd, buff, n);
+        nr_linii++;
+    }
+
+    if (tip == 0 || tip == 1)
+    {
+        n = sprintf(buff, "Timpul: %s\n", data);
+        write(fd, buff, n);
+        nr_linii++;
+
+        n = sprintf(buff, "Contor legaturi: %d\n", nrLegaturi);
+        write(fd, buff, n);
+        nr_linii++;
+    }
+
+    n = sprintf(buff, "Drepturi acces user: %s\n", drepturi[0]);
     write(fd, buff, n);
+    nr_linii++;
 
-    n=sprintf(buff, "Drepturi acces grupuri: %s\n", drepturi[1]);
+    n = sprintf(buff, "Drepturi acces grupuri: %s\n", drepturi[1]);
     write(fd, buff, n);
+    nr_linii++;
 
-    n=sprintf(buff, "Drepturi acces pentru altii: %s\n", drepturi[2]);
+    n = sprintf(buff, "Drepturi acces pentru altii: %s\n", drepturi[2]);
     write(fd, buff, n);
+    nr_linii++;
 
-    write(fd, "--------------------------------------------------\n", 51);
+    /// write(fd, "--------------------------------------------------\n", 51);
 
     close(fd);
+    return nr_linii;
 }
 
 int main(int argc, char *argv[])
 {
-    if(argc!=2)
+    if (argc != 3)
     {
-        printf("Usage: %s <director_intrare>\n", argv[0]);
+        printf("Usage: %s <director_intrare> <director_iesire>\n", argv[0]);
         exit(0);
     }
-    int l=strlen(argv[1]);
-    if(l<5){
-        printf("Usage: %s <director_intrare>\n", argv[0]);
+    int l = strlen(argv[1]);
+    if (l < 5)
+    {
+        printf("Usage: %s <director_intrare> <director_iesire>\n", argv[0]);
         exit(0);
     }
-    int fd=creat ("statistica.txt", S_IWUSR|S_IRUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
-    if(fd==-1)
-    {
-        perror("Eroare creare fisier statistici.");
-    }
-    close(fd);
 
     char *director = argv[1];
 
-    //parcurgem directorul
-    //pentru fiecare intrare, verificam daca e fisier normal, bmp, leg sim, sau dir
-    //in fiecare caz, in functie de tip, apelam diverse functii
+    // parcurgem directorul
+    // pentru fiecare intrare, verificam daca e fisier normal, bmp, leg sim, sau dir
+    // in fiecare caz, in functie de tip, apelam diverse functii
 
     DIR *dir = opendir(director);
-    if(dir == NULL){
+    if (dir == NULL)
+    {
         perror("Eroare deschidere director\n");
         exit(0);
     }
     struct dirent *intrare;
-    while((intrare = readdir(dir)) != NULL){
+    int nr_copii=0;
+    while ((intrare = readdir(dir)) != NULL)
+    {
         char denumire[200];
         strcpy(denumire, director);
         strcat(denumire, "/");
         strcat(denumire, intrare->d_name);
         int tip = citireInfoFisier(denumire);
-        if(tip == 1){//fisier bmp
+        if (tip == 1)
+        { // fisier bmp
             citireBMP(denumire);
         }
-        scriereStatistici(denumire, tip);
+        int pid;
+        if ((pid = fork()) < 0)
+        {
+            perror("Eroare");
+            exit(1);
+        }
+        if (pid == 0)
+        {
+            /* codul fiului - doar codul fiului poate ajunge aici (doar in fiu fork() returneaza 0)*/
+
+            char cale[100];
+            strcpy(cale, argv[2]);
+            strcat(cale, "/");
+            strcat(cale, intrare->d_name);
+            strcat(cale, "_statistica.txt");
+
+            int nr = scriereStatistici(cale, denumire, tip);
+
+            exit(0); // apel necesar pentru a se opri codul fiului astfel incat acesta sa nu execute si codul parintelui
+        }
+        /* codul parintelui */
+        nr_copii++;
     }
-    if(closedir(dir) == -1){
+    if (closedir(dir) == -1)
+    {
         perror("Eroare inchidere director\n");
         exit(0);
     }
+    while(nr_copii)
+    {
+        int status;
+        int pid = wait(&status);
+        char mesaj[100];
+        int n = sprintf(mesaj, "S-a încheiat procesul cu pid-ul %d și codul %d.\n", pid, status);
+        write(1,mesaj,n);
+        nr_copii--;
+    }
+
     return 0;
-
 }
-
